915637fa3d01d9930ce42dba5e633e2a7b0c34bc
18tejaswini <tejaswini.a98@gmail.com>
2025-04-23 11:06:31 -0500

    SQL changes which include Users Schema for Login

--- sql/chatbot_schema.sql
+++ sql/chatbot_schema.sql
@@ -2,6 +2,7 @@ CREATE DATABASE ChatbotDB
 
 USE ChatbotDB
 
+--ChatHistory Schema (Stores the chat history along with scores for all users)
 CREATE TABLE ChatHistory (
     Id INT PRIMARY KEY IDENTITY(1,1),
     UserId VARCHAR(100),
@@ -11,5 +12,13 @@ CREATE TABLE ChatHistory (
     AvgDistress FLOAT,
     Timestamp DATETIME DEFAULT GETDATE()
 )
-
 SELECT * FROM ChatHistory
+
+--Users Schema (Stores user information for Login)
+CREATE TABLE Users (
+    UserId VARCHAR(100) PRIMARY KEY,
+    PasswordHash VARCHAR(256)
+)
+INSERT INTO Users (UserId, PasswordHash)
+VALUES ('doe@gmail.come', 'Password')
+SELECT * FROM Users
